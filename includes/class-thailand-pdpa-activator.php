de2523ef6204d94db5da5ed7a048eb75ca2e1b02
surakrai <surakraisam@gmail.com>
2021-04-03 09:38:23 +0700

    สร้างตาราง  tpdpa_logs,  tpdpa_logmeta

--- includes/class-thailand-pdpa-activator.php
+++ includes/class-thailand-pdpa-activator.php
@@ -20,6 +20,10 @@
  * @subpackage Thailand_Pdpa/includes
  * @author     https://www.surakrai.com <surakraisam@gmail.com>
  */
+
+global $tpdpa_db_version;
+$tpdpa_db_version = '1.0';
+
 class Thailand_Pdpa_Activator {
 
 	/**
@@ -30,6 +34,36 @@ class Thailand_Pdpa_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+
+		global $wpdb;
+		global $tpdpa_db_version;
+
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_tpdpa_logs = $wpdb->prefix . 'tpdpa_logs';
+		$table_tpdpa_logmeta = $wpdb->prefix . 'tpdpa_logmeta';
+
+		$sql_tpdpa_logs = "CREATE TABLE $table_tpdpa_logs (
+			log_id mediumint(9) NOT NULL AUTO_INCREMENT,
+			time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+			user_id mediumint(9),
+			ip_address varchar(55) DEFAULT '' NOT NULL,
+			PRIMARY KEY  (log_id)
+		) $charset_collate;";
+
+		
+		$sql_tpdpa_logmeta = "CREATE TABLE $table_tpdpa_logmeta (
+			meta_id mediumint(9) NOT NULL AUTO_INCREMENT,
+			log_id mediumint(9),
+			meta_key varchar(55) DEFAULT '' NOT NULL,
+			meta_value varchar(55) DEFAULT '' NOT NULL,
+			PRIMARY KEY  (meta_id)
+		) $charset_collate;";
+			
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql_tpdpa_logs );
+		dbDelta( $sql_tpdpa_logmeta );
+
+		add_option( 'tpdpa_db_version', $tpdpa_db_version );
 
 	}
 
